d6c1ca05aeac3e2f900e91c4b98f80a4ce4dacf5
Hoang Tran <36368107+HoangTran0410@users.noreply.github.com>
2020-04-23 13:30:27 +0700

    add hide, show utils

--- js/utils/Dom.js
+++ js/utils/Dom.js
@@ -1,6 +1,11 @@
 const gid = (id) => document.getElementById(id);
 const gcl = (className) => document.getElementsByClassName(className)[0];
 
+const hide = (ele) => (ele.style.display = 'none');
+const show = (ele) => (ele.style.display = 'initial');
+
 export default {
+  hide,
+  show,
   loadingDiv: gid('loading'),
 };
