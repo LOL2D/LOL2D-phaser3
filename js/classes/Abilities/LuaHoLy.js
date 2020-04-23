dd56b4db621dc27da3b2615bdc70b1d8f2646cbc
Hoang Tran <36368107+HoangTran0410@users.noreply.github.com>
2020-04-23 13:31:39 +0700

    Update LuaHoLy.js

--- js/classes/Abilities/LuaHoLy.js
+++ js/classes/Abilities/LuaHoLy.js
@@ -1,5 +1,16 @@
 class LuaHoLy {
-  constructor() {}
+  constructor(champion) {
+    this.champion = champion;
+  }
 }
 
-LuaHoLy.name = 'Lửa Hồ Ly';
+LuaHoLy.infomation = {
+  name: 'Lửa Hồ Ly',
+  description: [
+    'Ahri phóng ra rồi thu hồi quả cầu ma thuật của cô, gây sát thương phép trên đường bay ra và sát thương chuẩn trên đường thu về. Sau khi dùng phép trúng vài lần, quả cầu trúng địch kế tiếp sẽ hồi máu cho Ahri.',
+
+    'Gây 40/65/90/115/140 (+) sát thương phép trên đường đi và 40/65/90/115/140 (+) sát thương chuẩn trên đường về.',
+
+    'Các kỹ năng của Ahri tích cộng dồn Hút Hồn khi trúng kẻ địch (tối đa mỗi lần tung chiêu). Ở cộng dồn, Quả Cầu Ma Thuật kế tiếp của Ahri hồi cho cô (+) máu mỗi khi trúng 1 kẻ địch.',
+  ],
+};
