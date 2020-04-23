d8dacab05e4f0dd97cb87bbd5bb35311841fe6a4
Hoang Tran <36368107+HoangTran0410@users.noreply.github.com>
2020-04-23 13:31:28 +0700

    test GameData

--- js/scenes/Menu.js
+++ js/scenes/Menu.js
@@ -1,10 +1,18 @@
+import { getAsset } from '../stores/GameData.js';
+
 const Menu = function (_) {
+  let yasuo;
+
   this.enter = function () {
-    console.log('Menu');
+    _.imageMode(_.CENTER);
+
+    yasuo = getAsset('yasuo');
   };
 
   this.draw = function () {
     _.background(30);
+
+    _.image(yasuo, _.width / 2, _.height / 2);
   };
 };
 
