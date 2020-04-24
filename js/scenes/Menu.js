fa4418628c52cc9fcc0af0fef5ea72d63ce44a80
Hoang Tran <36368107+HoangTran0410@users.noreply.github.com>
2020-04-24 11:27:48 +0700

    "the project is under development" notification

--- js/scenes/Menu.js
+++ js/scenes/Menu.js
@@ -1,18 +1,26 @@
 import { getAsset } from '../stores/GameData.js';
+import { Ahri } from '../classes/champions/Ahri.js';
 
 const Menu = function (_) {
-  let yasuo;
+  let yasuo, arhi;
 
   this.enter = function () {
     _.imageMode(_.CENTER);
+    _.textAlign(_.CENTER, _.CENTER);
+    _.textSize(20);
+    _.fill(255);
+
+    arhi = new Ahri();
+    console.log(arhi);
 
     yasuo = getAsset('yasuo');
   };
 
   this.draw = function () {
-    _.background(30);
-
     _.image(yasuo, _.width / 2, _.height / 2);
+
+    _.background(30, 200);
+    _.text('The project is under development', _.width / 2, _.height / 2);
   };
 };
 
