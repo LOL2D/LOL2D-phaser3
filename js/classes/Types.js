90ef16d6db2e2b00c8de55cedbced2731f27d122
Hoang Tran <36368107+HoangTran0410@users.noreply.github.com>
2020-04-25 23:57:11 +0700

    objects classification

---
+++ js/classes/Types.js
@@ -0,0 +1,16 @@
+export const Types = {
+  // Nhân vật
+  Champion: 'Champion',
+  Minion: 'Minion',
+
+  // Những vật thể tạo ra từ chiêu thức: boom từ R của jinx, trói ánh sáng từ Q của lux, ...
+  AbilityObject: 'AbilityObject',
+
+  // Các items trong cửa hàng trong trận đấu
+  Item: 'Item',
+
+  // type khác - chưa nghĩ ra công dụng cho việc lưu các type này, nhưng cứ lưu cho chắc
+  Ability: 'Ability',
+  Hud: 'Hud',
+  Wall: 'Wall'
+};
