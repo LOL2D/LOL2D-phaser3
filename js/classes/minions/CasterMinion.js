4fb95e6df254d7fb1ebc3b790f4ca1bdadbc64a5
Hoang Tran <36368107+HoangTran0410@users.noreply.github.com>
2020-04-25 23:57:00 +0700

    basic minions classification

---
+++ js/classes/minions/CasterMinion.js
@@ -0,0 +1,8 @@
+// https://leagueoflegends.fandom.com/wiki/Caster_minion
+import { Minion } from './Minion.js';
+
+class CasterMinion extends Minion {
+  constructor() {}
+}
+
+export { CasterMinion };
---
+++ js/classes/minions/SuperMinion.js
@@ -0,0 +1,8 @@
+// https://leagueoflegends.fandom.com/wiki/Super_minion
+import { Minion } from './Minion.js';
+
+class SuperMinion extends Minion {
+  constructor() {}
+}
+
+export { SuperMinion };
