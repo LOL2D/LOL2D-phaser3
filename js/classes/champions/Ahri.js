9b0367b52c58613032cff9f1820f3edb41c5efae
Hoang Tran <36368107+HoangTran0410@users.noreply.github.com>
2020-04-26 01:07:59 +0700

    update champion structure

--- js/classes/champions/Ahri.js
+++ js/classes/champions/Ahri.js
@@ -1,70 +1,90 @@
 // import * as abilities from '../abilities/Abilities.js';
+import { Classes } from './Classes.js';
+import { Champion } from './Champion.js';
 import { LuaHoLy } from '../abilities/Abilities.js';
 
-class Ahri {
+class Ahri extends Champion {
   constructor() {
-    console.log(Ahri.infomations);
+    super();
 
-    // for (let abi in abilities) {
-    //   console.log(abilities[abi]);
-    // }
+    this.stats = this.getStats(Ahri.default.stats);
+    this.abilities = this.getAbilities(Ahri.default.abilities);
+
+    console.log(this);
+  }
+
+  show() {
+    
   }
 }
 
 // static properties
-Ahri.infomations = {
+Ahri.default = {
   info: {
     name: 'Ahri',
-    nameVietNamese: 'Hồ ly chín đuôi',
+    nameVietNamese: 'Hồ Ly Chín Đuôi',
+    classes: Classes.Mage,
+    subClasses: [Classes.Mage.subs.Burst],
     homeTown: 'Ionia',
-    link: 'link/to/champion',
-    cost: 4800,
+    link: 'https://leagueoflegends.fandom.com/wiki/Ahri',
+    price: 4800,
   },
-  skins: [
-    {
+  skins: {
+    default: {
       name: 'Mặc định',
       background: 'path/to/background',
       avatar: 'path/to/avatar',
     },
-  ],
+    1: {},
+  },
+  score: {
+    // Điểm của từng chỉ số tướng trên thang 10 điểm
+    // Lấy từ: https://lienminh.fandom.com/wiki/Ahri
+    defense: 4,
+    attack: 3,
+    magic: 8,
+    difficulty: 5,
+  },
   stats: {
-    health: 618,
-    _health: 92, // + mỗi cấp
+    // Chỉ số cơ bản của tướng - dưới đây ví dụ là chỉ số của arhi
+    // lấy từ: https://lienminh.fandom.com/wiki/Ahri
+    health: 618, // máu
+    _health: 92, // máu cộng thêm mỗi cấp
 
-    mana: 443,
-    _mana: 25,
+    mana: 443, // năng lượng
+    _mana: 25, // năng lượng cộng thêm mỗi cấp
 
-    attackDamage: 56.04,
-    _attackDamage: 3,
+    attackDamage: 56.04, // sát thương
+    _attackDamage: 3, // sát thương cộng thêm mỗi cấp
 
-    magicalDamage: 0,
-    _magicalDamage: 0,
+    magicalDamage: 0, // sát thương phép
+    _magicalDamage: 0, // sát thương phép cộng thêm mỗi cấp
 
-    attackSpeed: 1000, // mili giay
-    _attackSpeed: 2, // + % mỗi cấp
+    attackSpeed: 0.668, // tốc độ đánh - hiện tại tính theo giây
+    _attackSpeed: 2, // tốc độ đánh cộng thêm mỗi cấp - cộng theo %
 
-    attackRange: 550,
+    attackRange: 550, // tầm đánh
 
-    moveSpeed: 330,
+    moveSpeed: 330, // tốc chạy
 
-    healthRegen: 7.1, // mỗi giây
-    _healthRegen: 0.6,
+    healthRegen: 7.1, // máu hồi phục (mỗi giây)
+    _healthRegen: 0.6, // máu hồi phục (mỗi giây) cộng thêm theo cấp
 
-    manaRegen: 8.8, // mỗi giây
-    _manaRegen: 0.8,
+    manaRegen: 8.8, // năng lượng hồi phục (mỗi giây)
+    _manaRegen: 0.8, // năng lượng hồi phục (mỗi giây) cộng them theo cấp
 
-    armor: 24.38,
-    _armor: 3.5,
+    armor: 24.38, // giáp
+    _armor: 3.5, // giáp cộng thêm theo cấp
 
-    magicalResist: 30.5,
-    _magicalResist: 0.5,
+    magicalResist: 30.5, // kháng phép
+    _magicalResist: 0.5, // kháng phép cộng thêm theo cấp
   },
   abilities: {
-    P: '', // passive - thụ động - nội tại
-    Q: LuaHoLy,
-    E: '',
-    W: '',
-    R: '',
+    passive: null, // passive - thụ động - nội tại
+    q: LuaHoLy,
+    w: null,
+    e: null,
+    r: null,
   },
 };
 
--- js/classes/champions/Champion.js
+++ js/classes/champions/Champion.js
@@ -1,5 +1,4 @@
 // https://leagueoflegends.fandom.com/wiki/List_of_champions
-import { Classes } from './Classes.js';
 import { Types } from '../Types.js';
 
 class Champion {
@@ -8,75 +7,30 @@ class Champion {
 
     this.type = Types.Champion;
   }
+
+  getStats(stats) {
+    const result_stats = {};
+    for (let stat in stats) {
+      result_stats[stat] = stats[stat];
+    }
+
+    return result_stats;
+  }
+
+  getAbilities(abilities) {
+    const result_abilities = {};
+    for (let ability in abilities) {
+      let c = abilities[ability];
+
+      if (typeof c === 'function') {
+        result_abilities[ability] = new c({ owner: this });
+      } else {
+        result_abilities[ability] = c;
+      }
+    }
+
+    return result_abilities;
+  }
 }
-
-Champion.default = {
-  info: {
-    name: "Champion's name",
-    nameVietNamese: 'Tên tướng trong tiếng việt',
-    classes: Classes.Controller,
-    subClasses: [Classes.Controller.subs.Catcher],
-    homeTown: 'Quê nhà',
-    link: 'https://leagueoflegends.fandom.com/wiki/List_of_champions',
-    price: 4800,
-  },
-  skins: {
-    default: {
-      name: 'Mặc định',
-      background: 'path/to/background',
-      avatar: 'path/to/avatar',
-    },
-    1: {},
-  },
-  score: {
-    // Điểm của từng chỉ số tướng trên thang 10 điểm
-    // Lấy từ: https://lienminh.fandom.com/wiki/Ahri
-    defense: 4,
-    attack: 3,
-    magic: 8,
-    difficulty: 5,
-  },
-  stats: {
-    // Chỉ số cơ bản của tướng - dưới đây ví dụ là chỉ số của arhi
-    // lấy từ: https://lienminh.fandom.com/wiki/Ahri
-    health: 618, // máu
-    _health: 92, // máu cộng thêm mỗi cấp
-
-    mana: 443, // năng lượng
-    _mana: 25, // năng lượng cộng thêm mỗi cấp
-
-    attackDamage: 56.04, // sát thương
-    _attackDamage: 3, // sát thương cộng thêm mỗi cấp
-
-    magicalDamage: 0, // sát thương phép
-    _magicalDamage: 0, // sát thương phép cộng thêm mỗi cấp
-
-    attackSpeed: 0.668, // tốc độ đánh - hiện tại tính theo giây
-    _attackSpeed: 2, // tốc độ đánh cộng thêm mỗi cấp - cộng theo %
-
-    attackRange: 550, // tầm đánh
-
-    moveSpeed: 330, // tốc chạy
-
-    healthRegen: 7.1, // máu hồi phục (mỗi giây)
-    _healthRegen: 0.6, // máu hồi phục (mỗi giây) cộng thêm theo cấp
-
-    manaRegen: 8.8, // năng lượng hồi phục (mỗi giây)
-    _manaRegen: 0.8, // năng lượng hồi phục (mỗi giây) cộng them theo cấp
-
-    armor: 24.38, // giáp
-    _armor: 3.5, // giáp cộng thêm theo cấp
-
-    magicalResist: 30.5, // kháng phép
-    _magicalResist: 0.5, // kháng phép cộng thêm theo cấp
-  },
-  abilities: {
-    passive: 'ClassA', // passive - thụ động - nội tại
-    q: 'ClassB',
-    w: 'ClassC',
-    e: 'ClassD',
-    r: 'ClassE',
-  },
-};
 
 export { Champion };
