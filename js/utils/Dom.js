d8d2e66ff965c243311548ec374085973f4f89ad
Hoang Tran <36368107+HoangTran0410@users.noreply.github.com>
2020-04-27 19:59:59 +0700

    update Dom utils

--- js/utils/Dom.js
+++ js/utils/Dom.js
@@ -1,11 +1,20 @@
 const gid = (id) => document.getElementById(id);
 const gcl = (className) => document.getElementsByClassName(className)[0];
+const qs = (str) => document.querySelector(str);
 
 const hide = (ele) => (ele.style.display = 'none');
 const show = (ele) => (ele.style.display = 'initial');
 
-export default {
-  hide,
-  show,
-  loadingDiv: gid('loading'),
+export const Dom = {
+  utils: {
+    hide,
+    show,
+  },
+  elements: {
+    loadingDiv: qs('#loading'),
+
+    // menu scene
+    // menuDiv: qs('#menu'),
+    // btnPlay: qs('#menu button'),
+  },
 };
