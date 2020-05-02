9eac059aba652a3949d3f4590ed5e5489973a951
Hoang Tran <99.hoangtran@gmail.com>
2020-05-02 22:35:01 +0700

    add camera

--- js/stores/Global.js
+++ js/stores/Global.js
@@ -1,8 +1,9 @@
 export const Global = {
   assets: {},
-  players: [
-    // Champion: class
-  ],
-  ability_objects: [], // Store all objects that has been launched by abilities
   p5, // instant variable of p5.js
+
+  // ingame settings
+  players: [],
+  ability_objects: [], // Store all objects that has been launched by abilities
+  camera: null,
 };
