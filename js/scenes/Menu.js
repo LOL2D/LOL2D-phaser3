a287ba8499ab8eac4fa14e9f6bdc4ca25a74f3f0
Hoang Tran <99.hoangtran@gmail.com>
2020-05-02 22:36:07 +0700

    remove champ test

--- js/scenes/Menu.js
+++ js/scenes/Menu.js
@@ -1,24 +1,26 @@
 import Champions from '../classes/champions/Champions.js';
 import { Global } from '../stores/Global.js';
 
-const Menu = function () {
-  let ahri, ahri2;
+import { InGame } from "./InGame.js";
 
+const Menu = function () {
   this.enter = function () {
     console.log(Champions);
-
-    ahri = new Champions.Ahri();
-    ahri2 = new Champions.Ahri();
+    
+    for(let champ in Champions) {
+      console.log(champ)
+    }
   };
 
   this.draw = function () {
     Global.p5.background(0);
-    ahri.show(Global.p5);
-    ahri2.show(Global.p5);
+
+    Global.p5.fill(255);
+    Global.p5.text('Click to play', Global.p5.width / 2, Global.p5.height / 2);
   };
 
   this.keyPressed = function () {
-    ahri.Q();
+    this.sceneManager.showScene(InGame);
   };
 };
 
