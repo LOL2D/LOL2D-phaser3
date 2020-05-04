4819ef6c37c96427c6a3f90829676ab06d4abde7
Hoang Tran <99.hoangtran@gmail.com>
2020-05-04 21:05:08 +0700

    remove all unnecessary code

--- src/js/stores/Global.js
+++
@@ -1,9 +0,0 @@
-export const Global = {
-  assets: {},
-  p5, // instant variable of p5.js
-
-  // ingame settings
-  players: [],
-  ability_objects: [], // Store all objects that has been launched by abilities
-  camera: null,
-};
