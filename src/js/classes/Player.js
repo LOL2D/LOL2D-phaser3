d2e2e1520a4223e1014bfa067844442803c40002
Hoang Tran <hoang.tran@youthdev.net>
2020-08-30 23:24:33 +0700

    change to phaser 3

--- src/js/classes/Player.js
+++ src/js/classes/Player.js
@@ -1,15 +1,37 @@
-class Player {
+import Phaser from 'phaser';
+
+export default class Player {
   constructor(config) {
     const {
+      x = 0,
+      y = 0,
       name = 'Player',
       money = 0,
       items = [],
       level = 1,
       exp = 0,
+      scene,
     } = config;
+
+    this.scene = scene;
+    this.x = x;
+    this.y = y;
+
+    this.init();
   }
 
-  control(champion) {}
-}
+  init() {
+    this.scene.input.on(Phaser.Input.Events.POINTER_DOWN, (e) => {
+      this.x = e.x;
+      this.y = e.y;
+    });
+  }
 
-export { Player };
+  setChampion(champion) {
+    this.champion = champion;
+  }
+
+  control() {}
+
+  update() {}
+}
