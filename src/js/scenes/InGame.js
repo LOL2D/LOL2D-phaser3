c6105bf2cf60936dba588c9ea09ec6afb9b502fd
Lưu Minh Hoàng <ceriagame@gmail.com>
2020-09-08 13:46:00 +0700

    InGame: added doCreatePlayer, commented out logo and yasuo

--- src/js/scenes/InGame.js
+++ src/js/scenes/InGame.js
@@ -8,23 +8,26 @@ export default class InGame extends Phaser.Scene {
     super({ key: SCENES.INGAME });
   }
 
-  // init() {}
+  // -- LIFECYCLE --
+  init() {
+    this.centerX = this.game.config.width / 2;
+    this.centerY = this.game.config.height / 2;
+  }
 
   // preload() {}
 
   create() {
-    const centerX = this.game.config.width / 2;
-    const centerY = this.game.config.height / 2;
-
     this.input.setDefaultCursor(`url(${NormalCursor}), default`);
+
+    const player = this.doCreatePlayer();
 
     // this.player = new Player({
     //   scene: this,
     // });
 
     const text = this.add.bitmapText(
-      centerX,
-      centerY,
+      this.centerX,
+      this.centerY,
       FONTS.PIXEL,
       'Coming Soon',
       16,
@@ -33,49 +36,47 @@ export default class InGame extends Phaser.Scene {
     text.setOrigin(0.5);
 
     // physic effect
-    const particles = this.add.particles('BlueDot');
+    // const particles = this.add.particles('BlueDot');
 
-    const emitter = particles.createEmitter({
-      speed: { min: 20, max: 150 },
-      scale: { start: 1, end: 0 },
-      lifespan: { min: 500, max: 2000 },
-      blendMode: 'ADD',
-    });
+    // const emitter = particles.createEmitter({
+    //   speed: { min: 20, max: 150 },
+    //   scale: { start: 1, end: 0 },
+    //   lifespan: { min: 500, max: 2000 },
+    //   blendMode: 'ADD',
+    // });
 
-    const size = 75;
+    // const size = 75;
 
-    const logo = this.physics.add.image(400, 100, 'LolLogo');
-    logo.body.setCircle(size);
-    logo
-      .setSize(size, size)
-      .setDisplaySize(size, size)
-      .setVelocity(100, 200)
-      .setBounce(1, 1)
-      .setCollideWorldBounds(true);
+    // const logo = this.physics.add.image(400, 100, 'LolLogo');
+    // logo.body.setCircle(size);
+    // logo
+    //   .setSize(size, size)
+    //   .setDisplaySize(size, size)
+    //   .setVelocity(100, 200)
+    //   .setBounce(1, 1)
+    //   .setCollideWorldBounds(true);
 
-    emitter.startFollow(logo);
+    // emitter.startFollow(logo);
 
-    // collide with mouse effect
-    const yasuo = this.physics.add.image(400, 400, 'Yasuo');
-    yasuo.body.setCircle(size);
-    yasuo
-      .setSize(size, size)
-      .setDisplaySize(size, size)
-      .setBounce(1, 1)
-      .setCollideWorldBounds(true);
+    // // collide with mouse effect
+    // const yasuo = this.physics.add.image(400, 400, 'Yasuo');
+    // yasuo.body.setCircle(size);
+    // yasuo
+    //   .setSize(size, size)
+    //   .setDisplaySize(size, size)
+    //   .setBounce(1, 1)
+    //   .setCollideWorldBounds(true);
 
-    this.physics.add.collider(logo, yasuo);
-
-    this.input.on(Phaser.Input.Events.POINTER_MOVE, (pointer) => {
-      this.physics.moveToObject(yasuo, pointer, 150);
-    });
+    // this.input.on(Phaser.Input.Events.POINTER_MOVE, (pointer) => {
+    //   this.physics.moveToObject(yasuo, pointer, 150);
+    // });
 
     // fade in effect
     const rect = this.add.rectangle(
-      centerX,
-      centerY,
-      centerX * 2,
-      centerY * 2,
+      this.centerX,
+      this.centerY,
+      this.centerX * 2,
+      this.centerY * 2,
       COLORS.GRAY1
     );
     this.tweens.add({
@@ -88,5 +89,18 @@ export default class InGame extends Phaser.Scene {
         rect.destroy();
       },
     });
+
+    // Initialize Colliders
+    this.physics.add.collider([player]);
+  }
+
+  // -- TEMPORARY FUNCTIONS --
+
+  doCreatePlayer() {
+    const player = this.add.circle();
+    player.setPosition(this.centerX, this.centerY);
+    player.setRadius(25);
+    player.setFillStyle(0xff5f00, 1);
+    return player;
   }
 }
